a302a46d70b965e8529825cc49c43f489615d3ec
thaymerapv <thaymerapv@gmail.com>
2021-04-10 17:14:53 -0400

    ipfs files upload

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
 const express = require('express');
+const multer = require('multer');
+const storage = multer.memoryStorage()
+const upload = multer({storage}).array('file', 20)
 const app = express();
+const {ipfs} = require('../utils/helpers');
+let node;
+
+ipfs.on('ipfs', (data) => {
+    node = data;
+})
 
 app.get('/', (req, res, next) => {
     res.status(200).json({
         ok: true,
-        message:'Repository for testing with ipfs',
+        message: 'Repository for testing with ipfs',
         author: 'BandicootDev'
     })
 })
@@ -13,8 +23,35 @@ app.get('/file', (req, res, next) => {
 
 });
 
-app.post('/file', (req, res, next) => {
+app.post('/file', [upload, async (req, res, next) => {
+    await node.ready;
+    let promises;
 
-});
+    if (!req.files) {
+        return res.status(400).json({
+            ok: false,
+            message: 'Bad Request'
+        })
+    }
+
+    const files = [...req.files]
+
+    try {
+        console.time('load result in ipfs');
+        promises = await Promise.all(files.map((file) => {
+            return node.add(file.buffer)
+        }))
+        console.timeEnd('load result in ipfs');
+    } catch (err) {
+        next(err)
+    }
+    console.log(promises)
+
+    res.status(200).json({
+        ok: true,
+        message: 'files uploaded successfully'
+    })
+}]);
+
 
 module.exports = app;
